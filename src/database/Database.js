e7b801439e56ddc166a93aa14a2f13092c483486
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-03-31 13:58:29 -0400

    Delete Database.js

--- src/database/Database.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://daaceyayqplykibiooqs.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYWNleWF5cXBseWtpYmlvb3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDcyNzYxNjEsImV4cCI6MTk2Mjg1MjE2MX0.VloR0wEBZavhqX_52sJAkzPzROtuGl2UfrpqFuXeFkE'
-const supabase = createClient(supabaseUrl, supabaseKey)
